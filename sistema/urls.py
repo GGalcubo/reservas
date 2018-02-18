eab979abcf07bad34e9d61335f78e36cf36c2880
jerecapo <jerecapo@gmail.com>
2018-02-18 15:14:55 -0300

    putos

--- sistema/urls.py
+++ sistema/urls.py
@@ -3,6 +3,6 @@ from sistema.views import dashboard
 
 urlpatterns = [
     url(r'^dashboard/', dashboard, name='dashboard'),
-    url(r'^operaciones/', dashboard, name='operaciones'),
+    url(r'^operaciones/', operaciones, name='operaciones'),
     #url(r'^$', 'sistema.views.index', name='index'),
 ]
