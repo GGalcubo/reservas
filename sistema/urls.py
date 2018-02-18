87e58cbcede8f5b2edbd8abd3751ccc97972b5e2
Lucio <lucio.bosco@gmail.com>
2018-02-18 15:17:43 -0300

    Operaciones importado

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from sistema.views import dashboard
+from sistema.views import dashboard, operaciones
 
 urlpatterns = [
     url(r'^dashboard/', dashboard, name='dashboard'),
