0391b0ad6f9cce5badbf7070b9f97e6c4f62686a
jerecapo <jerecapo@gmail.com>
2018-03-04 13:49:28 -0300

    viaje y estado, primera parte, con sus admin. hacer makemigrations sistema y luego migrate.

--- sistema/admin.py
+++ sistema/admin.py
@@ -2,6 +2,27 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import Permission
 from django.contrib import admin
+from .models import Viaje, Estado
 
 # Register your models here.
+class EstadoAdmin(admin.ModelAdmin):
+    
+    list_display = ('estado',)
+    fieldsets = (
+        ('Principal', {
+            'fields': ['estado']
+        }),
+    )
+
+class ViajeAdmin(admin.ModelAdmin):
+    
+    list_display = ('factura', 'proforma', 'estado', 'fecha', 'hora', 'base_total', 'peaje_total', 'estacionamiento_total', 'maletas', 'bilingue')
+    fieldsets = (
+        ('Principal', {
+            'fields': ['factura', 'proforma', 'estado', 'fecha', 'hora', 'base_total', 'peaje_total', 'estacionamiento_total', 'maletas', 'bilingue']
+        }),
+    )
+
+admin.site.register(Estado, EstadoAdmin)
+admin.site.register(Viaje, ViajeAdmin)
 admin.site.register(Permission)
--- sistema/models.py
+++ sistema/models.py
@@ -4,3 +4,37 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Estado(models.Model):
+    estado = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return u'%s' % self.estado
+
+    def __str__(self):
+        return self.estado
+
+class Viaje(models.Model):
+    factura = models.CharField(max_length=30, null=True, blank=True)
+    proforma = models.CharField(max_length=30, null=True, blank=True)
+    estado = models.ForeignKey(Estado)
+    fecha = models.CharField(max_length=8)
+    hora = models.CharField(max_length=4)
+    #empresa
+    #vehiculo
+    #observaciones
+    #adjuntos
+    base_total = models.IntegerField(default=0)
+    peaje_total = models.IntegerField()
+    estacionamiento_total = models.IntegerField()
+    Otros_tot = models.IntegerField()
+    maletas = models.CharField(max_length=30)
+    bilingue = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return u'%s' % self.fecha
+
+    def __str__(self):
+        return self.fecha
+
+    class Meta:
+        verbose_name_plural = "Viajes" 
