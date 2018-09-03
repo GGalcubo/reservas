dcba91e61d9ab0f8d8fe542ac114b3d3644c953d
Cristian <cristianoaltamirano@gmail.com>
2018-09-03 11:43:20 -0300

    saque comentarios

--- sistema/views.py
+++ sistema/views.py
@@ -221,18 +221,9 @@ def guardarViaje(request):
 
 @login_required
 def guardarViajeAdjunto(request):
-
-
-    # settings for the file upload
-    #   you can define other parameters here
-    #   and check validity late in the code
     options = {
-        # the maximum file size (must be in bytes)
         "maxfilesize": 2 * 2 ** 20,  # 2 Mb
-        # the minimum file size (must be in bytes)
         "minfilesize": 1 * 2 ** 10,  # 1 Kb
-        # the file types which are going to be allowed for upload
-        #   must be a mimetype
         "acceptedformats": (
             "image/jpeg",
             "image/png",
@@ -240,27 +231,16 @@ def guardarViajeAdjunto(request):
     }
 
     if request.method == 'POST':
-
-
-        # used to generate random unique id
         import uuid
 
         if not request.POST[u"uid"]:
             return HttpResponseBadRequest("UID not specified.")
-            # if here, uid has been specified, so record it
         uid = request.POST[u"uid"]
 
-        # figure out the path where files will be uploaded to
-        # PROJECT_ROOT is from the settings file
         temp_path = os.path.join(settings.PROJECT_ROOT, "adjuntos")
         file = request.FILES[u'files[]']
 
-        # initialize the error
-        # If error occurs, this will have the string error message so
-        # uploader can display the appropriate message
         error = False
-
-        # check against options for errors
 
         # file size
         if file.size > options["maxfilesize"]:
@@ -271,32 +251,21 @@ def guardarViajeAdjunto(request):
         if file.content_type not in options["acceptedformats"]:
             error = "acceptFileTypes"
 
-        # the response data which will be returned to the uploader as json
         response_data = {
             "name": file.name,
             "size": file.size,
             "type": file.content_type
         }
 
-        # if there was an error, add error message to response_data and return
         if error:
-            # append error message
             response_data["error"] = error
-            # generate json
             response_data = json.dumps([response_data])
-            # return response to uploader with error
-            # so it can display error message
             return HttpResponse(response_data, mimetype='application/json')
 
-        # make temporary dir if not exists already
         if not os.path.exists(temp_path):
             os.makedirs(temp_path)
 
-        # get the absolute path of where the uploaded file will be saved
-        # all add some random data to the filename in order to avoid conflicts
-        # when user tries to upload two files with same filename
         filename = os.path.join('adjuntos/', str(uuid.uuid4()) + file.name)
-        #print filename
 
         print request.get_full_path() + file.name
         print settings.BASE_DIR
@@ -310,32 +279,14 @@ def guardarViajeAdjunto(request):
         viajeAdjunto.viaje_id = request.POST[u"idViaje"]
         viajeAdjunto.save()
 
-        # open the file handler with write binary mode
         destination = open(filename, "wb+")
-        # save file data into the disk
-        # use the chunk method in case the file is too big
-        # in order not to clutter the system memory
         for chunk in file.chunks():
             destination.write(chunk)
-            # close the file
         destination.close()
-        #viajeAdjunto.adjunto.upload = request.POST.get('idViaje', False)
-        #viajeAdjunto.viaje_id = request.POST.get('idViaje', False)
-        # here you can add the file to a database,
-        #                           move it around,
-        #                           do anything,
-        #                           or do nothing and enjoy the demo
-        # just make sure if you do move the file around,
-        # then make sure to update the delete_url which will be send to the server
-        # or not include that information at all in the response...
-
-        # allows to generate properly formatted and escaped url queries
         import urllib
 
-        # url for deleting the file in case user decides to delete it
         response_data["delete_url"] = request.path + "?" + urllib.urlencode(
             {"f": uid + "/" + os.path.split(filename)[1]})
-        # specify the delete type - must be POST for csrf
         response_data["delete_type"] = "POST"
         response_data["url"] = settings.BASE_DIR + '/' + filename
         response_data["thumbnailUrl"] = settings.BASE_DIR + '/' + filename
@@ -344,20 +295,8 @@ def guardarViajeAdjunto(request):
             'files': [response_data]
         }
 
-        # generate the json data
         response_data = json.dumps(response_data)
-        # response type
         response_type = "application/json"
-
-        # QUIRK HERE
-        # in jQuey uploader, when it falls back to uploading using iFrames
-        # the response content type has to be text/html
-        # if json will be send, error will occur
-        # if iframe is sending the request, it's headers are a little different compared
-        # to the jQuery ajax request
-        # they have different set of HTTP_ACCEPT values
-        # so if the text/html is present, file was uploaded using jFrame because
-        # that value is not in the set when uploaded by XHR
         if "text/html" in request.META["HTTP_ACCEPT"]:
             response_type = "text/html"
 
