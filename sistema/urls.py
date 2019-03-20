508990a837e9fbbbde0c107b1c9ba7766b850143
jerecapo <jerecapo@gmail.com>
2019-03-20 20:36:40 -0300

    falto esto

--- sistema/urls.py
+++ sistema/urls.py
@@ -111,8 +111,6 @@ urlpatterns = [
     url(r'^cargarSolicitante/', cargarSolicitantes, name='cargarSolicitantes'),
     url(r'^cargarProveedores/', cargarProveedores, name='cargarProveedores'),
     url(r'^cargarMenu/', cargarMenu, name='cargarMenu'),
-    url(r'^urlBloqueada/', urlBloqueada, name='urlBloqueada'),
-    url(r'^error404/', error404, name='error404'),
-    url(r'^error500/', error500, name='error500')
+    url(r'^urlBloqueada/', urlBloqueada, name='urlBloqueada')
     #url(r'^$', 'sistema.views.index', name='index'),
 ]
